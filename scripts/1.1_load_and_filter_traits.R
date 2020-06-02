2f97bbd14bb08cd46256568baaa8fe50bb92b1fd
Rasmus <rasmus.iwfly@gmail.com>
2020-06-02 11:10:35 +0200

    Update scripts to Phylacine 1.2.1 and load new dens and fmr

--- scripts/1.1_load_and_filter_traits.R
+++ scripts/1.1_load_and_filter_traits.R
@@ -5,6 +5,17 @@ library(stringr)
 load("builds/current.maps.RData")
 load("builds/present.natural.maps.RData")
 
+library(tools)
+# Data from Phylacine v. 1.2.1
+sum(current.maps)
+# 1990604
+md5sum("builds/current.maps.RData")
+# "4669d8c2b5edba3f13ba4bf6d89903b0"
+sum(present.natural.maps)
+# 2210825
+md5sum("builds/present.natural.maps.RData")
+# "5bd2cc143ffd61c00aca3f41092c92e"
+
 # Load Phylacine trait data and remove all non terrestrial species
 df <- read_csv("../PHYLACINE_1.2/Data/Traits/Trait_data.csv", col_types = cols())
 bat.order <- "Chiroptera"
@@ -23,7 +34,7 @@ terrestrial <- df %>% filter(!Order.1.2 %in% c(bat.order, sea.cow.order),
 df <- df %>% filter(Binomial.1.2 %in% terrestrial)
 
 # Load FMR and check that we have FMR for all species
-fmr <- read_csv("../metabolic_rate/builds/imputed.metabolic.rate.csv", col_types = cols())
+fmr <- read_csv("../metabolic_rate/builds/Table S5 Imputed metabolic rate.csv", col_types = cols())
 all(df$Binomial.1.2 %in% fmr$Binomial.1.2)
 
 # Select important variables from FMR and join to df
@@ -32,7 +43,7 @@ fmr <- fmr %>% transmute(Binomial.1.2, log10fmr = log10.fmr.mean,
 df <- df %>% left_join(fmr, by = "Binomial.1.2")
 
 # Load animal density and check that we have density for all species
-dens <- read_csv("../predict_density/builds/imputed.density.csv", col_types = cols())
+dens <- read_csv("../mammal_density/output/Table S4 Imputed density.csv", col_types = cols())
 all(df$Binomial.1.2 %in% dens$Binomial.1.2)
 
 # Select important variables from dens and join to df

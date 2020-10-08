bcb8e5993eac1d561151399f0c0885b26bd5be0b
Rasmus <rasmus.iwfly@gmail.com>
2020-10-08 22:39:46 +0200

    Remove unused large dataframes from memory in load data

--- scripts/3.0_load.data.R
+++ scripts/3.0_load.data.R
@@ -1,3 +1,4 @@
+# Load libs for section 3.x
 library(tidyverse)
 library(tictoc)
 library(data.table)
@@ -10,54 +11,54 @@ library(gridExtra)
 # Load data
 df <- read_csv("builds/data.csv", col_types = cols())
 
+# Load consumption data
 consumption.samples <- read_csv("builds/sampled.consumption.distribution.kgC.yr.km2.csv", col_types = cols()) # [kgC / (km2 * year)]
-consumption.corrected <- read_csv("builds/species.consumption.kgC.yr.km2.csv", col_types = cols()) # [kgC / (km2 * year)]
-consumption.naive <- read_csv("builds/species.consumption.kgC.yr.km2.naive.csv", col_types = cols()) # [kgC / (km2 * year)]
 
-all(consumption$Binomial.1.2 == df$Binomial.1.2)
-consumption.corrected <- consumption.corrected %>% mutate(Q.plant = Q * df$Diet.Plant/100)
-
-consumption.summary <- consumption.samples %>% 
-  transmute(Binomial.1.2, 
+# Summarise consumption
+consumption.summary <- consumption.samples %>%
+  transmute(Binomial.1.2,
             mean = rowMeans(.[, -1]),
             median = apply(.[, -1], 1, median),
             ci.lw = apply(.[, -1], 1, quantile, probs = .025),
             ci.hi = apply(.[, -1], 1, quantile, probs = .975))
+# Alignment sanity check 
+stopifnot(all(consumption.summary$Binomial.1.2 == df$Binomial.1.2))
 
-ggplot(tibble(), aes(x = log10(Q))) +
-  geom_density(data = consumption.naive, col = "yellow") +
-  geom_density(data = consumption.corrected, col = "magenta") +
-  geom_density(data = consumption.summary, aes(x = log10(mean)), col = "red") +
-  geom_density(data = consumption.summary, aes(x = log10(median)), col = "darkgreen") +
-  geom_density(data = consumption.summary, aes(x = log10(ci.lw)), col = "darkgreen", lty = 2) +
-  geom_density(data = consumption.summary, aes(x = log10(ci.hi)), col = "darkgreen", lty = 2) +
-  theme_bw()
+# # Load mean models
+# consumption.corrected <- read_csv("builds/species.consumption.kgC.yr.km2.csv", col_types = cols()) # [kgC / (km2 * year)]
+# consumption.naive <- read_csv("builds/species.consumption.kgC.yr.km2.naive.csv", col_types = cols()) # [kgC / (km2 * year)]
+# 
+# # Check consumption models against each other
+# ggplot(tibble(), aes(x = log10(Q))) +
+#   geom_density(data = consumption.naive, col = "yellow") +
+#   geom_density(data = consumption.corrected, col = "magenta") +
+#   geom_density(data = consumption.summary, aes(x = log10(mean)), col = "red") +
+#   geom_density(data = consumption.summary, aes(x = log10(median)), col = "darkgreen") +
+#   geom_density(data = consumption.summary, aes(x = log10(ci.lw)), col = "darkgreen", lty = 2) +
+#   geom_density(data = consumption.summary, aes(x = log10(ci.hi)), col = "darkgreen", lty = 2) +
+#   theme_bw()
+# # We will use medians!
 
-all(consumption.summary$Binomial.1.2 == df$Binomial.1.2)
+# Estimate this in terms of plant consumtion
 consumption <- consumption.summary %>% mutate(Q.plant = median * df$Diet.Plant/100,
                                               ci.lw.plant = ci.lw * df$Diet.Plant/100,
                                               ci.hi.plant = ci.hi * df$Diet.Plant/100)
 
 
-# Load map data and builds consumption maps (this has to ben run after the load.maps.R script)
+# Load map data and builds consumption maps
 #load("builds/current.maps.filtered.RData")
-load("builds/current.maps.filtered.edge.lim.RData")
-current.maps <- current.maps.edge.lim
-rm(current.maps.edge.lim)
+current.maps <- readRDS("builds/current.maps.filtered.edge.lim.rds")
 current.consumption <- current.maps * consumption$Q.plant
 current.consumption.lw <- current.maps * consumption$ci.lw.plant
 current.consumption.hi <- current.maps * consumption$ci.hi.plant
-rm(current.maps) # Each loaded matrix is 1.7 GB ram
+rm(current.maps) # Each loaded matrix is 1.9 GB ram
 gc() # R forgets to clean up
 
-# load("builds/present.natural.maps.filtered.RData")
-load("builds/present.natural.maps.filtered.edge.lim.RData")
-present.natural.maps <- present.natural.maps.edge.lim
-rm(present.natural.maps.edge.lim)
+present.natural.maps <- readRDS("builds/present.natural.maps.filtered.edge.lim.rds")
 present.natural.consumption <- present.natural.maps * consumption$Q.plant
 present.natural.consumption.lw <- present.natural.maps * consumption$ci.lw.plant
 present.natural.consumption.hi <- present.natural.maps * consumption$ci.hi.plant
-rm(present.natural.maps) # Each loaded matrix is 1.7 GB ram
+rm(present.natural.maps) # Each loaded matrix is 1.9 GB ram
 gc() # R forgets to clean up
 
 base.map <- raster("builds/base_map.tif")
@@ -69,7 +70,7 @@ present.natural.consumption.map.lw <- base.map
 current.consumption.map.hi <- base.map
 present.natural.consumption.map.hi <- base.map
 
-# Summarise consumption per grid-cell
+# Summarize consumption per grid-cell
 # Transform change from [KgC / (km2 * year)] to [MgC / (km2 * year)]
 tic()
 current.consumption.map[] <- colSums(current.consumption) / 10^3
@@ -79,6 +80,10 @@ current.consumption.map.lw[] <- colSums(current.consumption.lw) / 10^3
 current.consumption.map.lw[current.consumption.map.lw == 0] <- NA
 current.consumption.map.hi[] <- colSums(current.consumption.hi) / 10^3
 current.consumption.map.hi[current.consumption.map.hi == 0] <- NA
+
+rm(current.consumption.lw)
+rm(current.consumption.hi)
+gc()
 toc()
 
 tic()
@@ -89,4 +94,9 @@ present.natural.consumption.map.lw[] <- colSums(present.natural.consumption.lw) 
 present.natural.consumption.map.lw[present.natural.consumption.map.lw == 0] <- NA
 present.natural.consumption.map.hi[] <- colSums(present.natural.consumption.hi) / 10^3
 present.natural.consumption.map.hi[present.natural.consumption.map.hi == 0] <- NA
+
+rm(present.natural.consumption.lw)
+rm(present.natural.consumption.hi)
+gc()
 toc()
+

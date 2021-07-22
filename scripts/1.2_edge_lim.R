f23611207a3515e789624a47868eb0d61e9cbc6b
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-22 16:29:50 +0200

    Clean Edge lim script, and move to SparseMatrix

--- scripts/1.2_edge_lim.R
+++ scripts/1.2_edge_lim.R
@@ -1,11 +1,17 @@
-# Load the filtered maps, and halve the range outline cells and save again
+# Load the filtered maps, and add a reduction factor to the range outline
+# 22/07-2021 Rasmus Ø Pedersen
 
-# Load libs
+# Load libraries
+library(tidyverse)
 library(raster)
+library(Matrix)
 
 # Load base map
 base.map <- raster("builds/base_map.tif")
 
+
+
+# Build a edge reduction function -----------------------------------------
 
 # Make a matrix of neighbour cells:
 rows <- nrow(base.map)
@@ -54,11 +60,18 @@ reduce_range_edge <- function(species.range) {
     # Rooks case neighbours + 1:
     j <- sum(species.range[neighbours[i, ]] > 0) + 1
     if(j == 5) next # Next; all here
+    # No neighbours, 1 neighbour, 2 neigbours, 3 neigbours reduction factors:
     reduction.factor <- c(1/9, 1/6, 1/4, 1/2)[j]
     species.range[i] <- species.range[i] * reduction.factor
   }
   return(species.range)
 }
+
+
+
+
+
+# Reduce ranges and store -------------------------------------------------
 
 # Load maps
 current.maps <- readRDS("builds/current.maps.filtered.rds")
@@ -66,10 +79,19 @@ present.natural.maps <- readRDS("builds/present.natural.maps.filtered.rds")
 
 # Reduce range edges and save maps
 current.maps.edge.lim <- t(apply(current.maps, 1, reduce_range_edge))
-saveRDS(current.maps.edge.lim, file = "builds/current.maps.filtered.edge.lim.rds")
+current.maps.edge.lim %>%
+  as("sparseMatrix") %>% 
+  write_rds(file = "builds/current.maps.filtered.edge.lim.rds")
 present.natural.maps.edge.lim <- t(apply(present.natural.maps, 1, reduce_range_edge))
-saveRDS(present.natural.maps.edge.lim, file = "builds/present.natural.maps.filtered.edge.lim.rds")
+present.natural.maps.edge.lim %>%
+  as("sparseMatrix") %>% 
+  write_rds(file = "builds/present.natural.maps.filtered.edge.lim.rds")
 
+
+
+
+# Check results -----------------------------------------------------------
+# 
 # # Plot maps with differences
 # pr <- par(mfrow = c(2, 2))
 # base.map[] <- colSums(present.natural.maps[])

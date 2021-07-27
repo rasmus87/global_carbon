3a8012a97660c462268d3a18123ab8b3b91afeaa
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-27 13:25:11 +0200

    Clean up script

--- scripts/3.0_load.data.R
+++ scripts/3.0_load.data.R
@@ -6,6 +6,8 @@ library(tidyverse)
 library(tictoc)
 library(Matrix)
 library(raster)
+library(sf)
+library(rnaturalearth)
 # library(data.table)
 # library(viridis)  # better colors for everyone
 # library(cowplot) # Load cowplot (has to be loaded before ggthemes!)
@@ -41,7 +43,10 @@ consumption <- consumption.summary %>%
          ci.hi.plant = ci.hi * df$Diet.Plant/100)
 
 
-# Load species maps and calculate consumption maps
+
+
+# Load species maps and calculate consumption maps ------------------------
+
 # Current maps
 current.maps <- read_rds("builds/current.maps.filtered.edge.lim.rds")
 current.consumption <- current.maps * consumption$Q.plant
@@ -57,6 +62,7 @@ present.natural.consumption.hi <- present.natural.maps * consumption$ci.hi.plant
 # Base map
 base.map <- raster("builds/base_map.tif")
 
+# Set up maps to use
 current.consumption.map <- base.map
 present.natural.consumption.map <- base.map
 current.consumption.map.lw <- base.map
@@ -64,40 +70,52 @@ present.natural.consumption.map.lw <- base.map
 current.consumption.map.hi <- base.map
 present.natural.consumption.map.hi <- base.map
 
+
+
+# Calculate total consumption per grid cell -------------------------------
 # Summarize consumption per grid-cell
 # Transform change from [KgC / (km2 * year)] to [MgC / (km2 * year)]
+
+# Current maps
 current.consumption.map[] <- colSums(current.consumption) / 10^3
 current.consumption.map[current.consumption.map == 0] <- NA
 
-megafauna <- which(df$Mass.g >= 45000)
-current.megafauna.consumption.map <- base.map
-current.megafauna.consumption.map[] <- colSums(current.consumption[megafauna, ]) / 10^3
-current.megafauna.consumption.map[current.megafauna.consumption.map == 0] <- NA
-
+# Current maps CI
 current.consumption.map.lw[] <- colSums(current.consumption.lw) / 10^3
 current.consumption.map.lw[current.consumption.map.lw == 0] <- NA
 current.consumption.map.hi[] <- colSums(current.consumption.hi) / 10^3
 current.consumption.map.hi[current.consumption.map.hi == 0] <- NA
 
+# Current megafauna maps
+megafauna <- which(df$Mass.g >= 45000)
+current.megafauna.consumption.map <- base.map
+current.megafauna.consumption.map[] <- colSums(current.consumption[megafauna, ]) / 10^3
+current.megafauna.consumption.map[current.megafauna.consumption.map == 0] <- NA
 
+
+# Present natural maps
 present.natural.consumption.map[] <- colSums(present.natural.consumption) / 10^3
 present.natural.consumption.map[present.natural.consumption.map == 0] <- NA
 
-megafauna <- which(df$Mass.g >= 45000)
-present.natural.megafauna.consumption.map <- base.map
-present.natural.megafauna.consumption.map[] <- colSums(present.natural.consumption[megafauna, ]) / 10^3
-present.natural.megafauna.consumption.map[present.natural.megafauna.consumption.map == 0] <- NA
-
+# Present natural maps CI
 present.natural.consumption.map.lw[] <- colSums(present.natural.consumption.lw) / 10^3
 present.natural.consumption.map.lw[present.natural.consumption.map.lw == 0] <- NA
 present.natural.consumption.map.hi[] <- colSums(present.natural.consumption.hi) / 10^3
 present.natural.consumption.map.hi[present.natural.consumption.map.hi == 0] <- NA
 
+# Present natural megafauna maps
+present.natural.megafauna.consumption.map <- base.map
+present.natural.megafauna.consumption.map[] <- colSums(present.natural.consumption[megafauna, ]) / 10^3
+present.natural.megafauna.consumption.map[present.natural.megafauna.consumption.map == 0] <- NA
+
+
+
+# Load world map overlay and set up plotting theme ------------------------
+
 # Load world shape overlay
-world.map <- getMap(resolution = "low")
-world.map <- unionSpatialPolygons(world.map, rep(1, nrow(world.map)))
-world.map <- spTransform(world.map, crs(base.map))
-world.map <- fortify(world.map)
+world.map <- ne_countries(returnclass = "sf") %>% 
+  st_transform(st_crs(base.map)) %>% 
+  st_union()
 
 # Create ggplot theme
 theme_R <- function() {
@@ -106,7 +124,10 @@ theme_R <- function() {
           axis.line = element_line(colour = "black"))
 }
 
-## Load Modis data of NPP
+
+
+# Load Modis data of NPP --------------------------------------------------
+
 # Modis data are based on leaf area and solar energy input
 # It is total NPP, and before _any_ consumption 
 # (other than what has been subtracted for respiration)
@@ -115,7 +136,11 @@ npp <- raster("../large_datasets/MOD17A3/NPP_projected_resampled_scaled_gCarbon_
 npp.cut <- raster("data/Q95_extreme_sd_npp1.tif")
 npp[which(npp.cut[] == 1)] <- NA
 
-## Cut of low NPP based on WWF Deserts and Xeric shrub
+
+
+
+# Cut off low NPP based on WWF Deserts and Xeric shrub ---------------------
+
 # Load WWF biomes
 wwf.biome <- base.map
 wwf.biome[] <- raster("data/wwf_terr_ecos_biome_raster.tif")[]
@@ -158,11 +183,14 @@ present.natural.consumption.map[remove.areas] <- NA
 current.megafauna.consumption.map[remove.areas] <- NA
 present.natural.megafauna.consumption.map[remove.areas] <- NA
 
+
+
+# Prepare datasets for plotting ----------------------------------
+
 # Calculate change between CU and PN
 change <- (current.consumption.map/present.natural.consumption.map - 1) * 100
 # Truncate positive change
 change[change > 0] <- 0
-
 
 # NPP consumption (Carbon consumed / Carbon produced) [%]
 # Find the fraction
@@ -183,52 +211,88 @@ present.natural.megafauna.npp.use[present.natural.megafauna.npp.use[] >= 100.5] 
 change.pct.point <- current.npp.use - present.natural.npp.use
 change.pct.point[] <- ifelse(current.npp.use[] == 101 | present.natural.npp.use[] == 101 , 101, change.pct.point[])
 
+
 # Turn carbon use into data.frames for plotting (all)
-current.consumption.df <- as(current.consumption.map, "SpatialPixelsDataFrame") # [MgC / km2 / year]
-current.consumption.df <- as_tibble(current.consumption.df)
-colnames(current.consumption.df) <- c("value", "x", "y")
-current.consumption.df <- current.consumption.df %>% mutate(period = "Current")
+# [MgC / km2 / year]
 
-present.natural.consumption.df <- as(present.natural.consumption.map, "SpatialPixelsDataFrame")
-present.natural.consumption.df <- as_tibble(present.natural.consumption.df)
-colnames(present.natural.consumption.df) <- c("value", "x", "y")
-present.natural.consumption.df <- present.natural.consumption.df %>% mutate(period = "Present natural")
+# Current
+current.consumption.df <- current.consumption.map %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Current")
 
-consumption.df <- bind_rows(current.consumption.df, present.natural.consumption.df)
+# Present natural
+present.natural.consumption.df <- present.natural.consumption.map %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Present natural")
+
+# Combined
+consumption.df <- bind_rows(current.consumption.df, 
+                            present.natural.consumption.df)
+
 
 # Turn NPP use into data.frames for plotting (all)
-current.npp.use.df <- as(current.npp.use, "SpatialPixelsDataFrame")
-current.npp.use.df <- as_tibble(current.npp.use.df)
-colnames(current.npp.use.df) <- c("value", "x", "y")
+# [%]
 
-present.natural.npp.use.df <- as(present.natural.npp.use, "SpatialPixelsDataFrame")
-present.natural.npp.use.df <- as_tibble(present.natural.npp.use.df)
-colnames(present.natural.npp.use.df) <- c("value", "x", "y")
+# Current
+current.npp.use.df <- current.npp.use %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Current")
 
-# Combine data.sets (all)
-pn.npp.use <- present.natural.npp.use.df %>% mutate(period = "Present natural")
-cu.npp.use <- current.npp.use.df %>% mutate(period = "Current")
-npp.use <- bind_rows(pn.npp.use, cu.npp.use)
-npp.use$period <- as.factor(npp.use$period)
-npp.use$period <- fct_relevel(npp.use$period, "Present natural")
+# Present natural
+present.natural.npp.use.df <- present.natural.npp.use %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Present natural")
+
+# Combined
+npp.use <- bind_rows(current.npp.use.df, 
+                     present.natural.npp.use.df)
+
 
 # Turn NPP use into data.frames for plotting (megafauna)
-current.megafauna.npp.use.df <- as(current.megafauna.npp.use, "SpatialPixelsDataFrame")
-current.megafauna.npp.use.df <- as_tibble(current.megafauna.npp.use.df)
-colnames(current.megafauna.npp.use.df) <- c("value", "x", "y")
+# [%]
 
-present.natural.megafauna.npp.use.spdf <- as(present.natural.megafauna.npp.use, "SpatialPixelsDataFrame")
-present.natural.megafauna.npp.use.df <- as_tibble(present.natural.megafauna.npp.use.spdf)
-colnames(present.natural.megafauna.npp.use.df) <- c("value", "x", "y")
+# Current
+current.megafauna.npp.use.df <- current.megafauna.npp.use %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Current")
 
-# Combine data.sets (megafauna)
-pn.megafauna.npp.use <- present.natural.megafauna.npp.use.df %>% mutate(period = "Present natural")
-cu.megafauna.npp.use <- current.megafauna.npp.use.df %>% mutate(period = "Current")
-megafauna.npp.use <- bind_rows(pn.megafauna.npp.use, cu.megafauna.npp.use)
-megafauna.npp.use$period <- as.factor(megafauna.npp.use$period)
-megafauna.npp.use$period <- fct_relevel(megafauna.npp.use$period, "Present natural")
+# Present natural
+present.natural.megafauna.npp.use.df <- present.natural.megafauna.npp.use %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Present natural")
 
-## Last of the wild:
+# Combined
+megafauna.npp.use <- bind_rows(current.megafauna.npp.use.df, 
+                               present.natural.megafauna.npp.use.df)
+
+
+
+# Last of the wild --------------------------------------------------------
+
 # Load and force align to basemap of LTW realms
 ltw.realm <- base.map
 ltw.realm[] <- raster("data/ltw_v2geo_realm.tif")[]
@@ -258,8 +322,12 @@ ltw <- ltw[, -1] %>%
   left_join(npp.use, by = c("x", "y"))
 
 
-# Summarise NPP use (all)
-(npp.use.sumamry <- npp.use %>% 
+
+
+# Calculate global consumption summaries ----------------------------------
+
+# Summarise NPP use (all) [%]
+(npp.use.summary <- npp.use %>% 
   group_by(period) %>%
   summarise(mean = mean(value, na.rm = T) %>% signif(2),
             sd = sd(value, na.rm = T) %>% signif(2),
@@ -267,8 +335,12 @@ ltw <- ltw[, -1] %>%
             q.025 = quantile(value, .025, na.rm = T) %>% signif(2),
             q.975 = quantile(value, .975, na.rm = T) %>% signif(2),
             n = n()))
+# PN use is x times higher than CU use
+npp.use.summary[[4]][2]/npp.use.summary[[4]][1]
+# Which is x % reduction
+signif(diff(npp.use.summary[[4]])/npp.use.summary[[4]][2] * 100, 2)
 
-# Summarise NPP use (megafauna)
+# Summarise NPP use (megafauna) [%]
 (megafauna.npp.use.summary <- megafauna.npp.use %>% 
   group_by(period) %>%
   summarise(mean = mean(value, na.rm = T) %>% signif(2),
@@ -278,16 +350,24 @@ ltw <- ltw[, -1] %>%
             q.975 = quantile(value, .975, na.rm = T) %>% signif(2),
             n = n()))
 
-# Megafauna NPP use percentage:
-signif(megafauna.npp.use.summary$mean[1]/npp.use.sumamry$mean[1] * 100, 2)
-signif(megafauna.npp.use.summary$mean[2]/npp.use.sumamry$mean[2] * 100, 2)
+# Megafauna NPP use compared to all fauna:
+# Current
+# all
+npp.use.summary$mean[1]
+# percentage used by megafauna
+signif(megafauna.npp.use.summary$mean[1]/npp.use.summary$mean[1] * 100, 2)
+# Present natural
+# all
+npp.use.summary$mean[2]
+# percentage used by megafauna
+signif(megafauna.npp.use.summary$mean[2]/npp.use.summary$mean[2] * 100, 2)
 
-# Magafauna NPP use drop percentage:
-overall.drop <- diff(npp.use.sumamry$mean)
+# Magafauna NPP use drop [%]:
+overall.drop <- diff(npp.use.summary$mean)
 megafauna.drop <- diff(megafauna.npp.use.summary$mean)
 signif(megafauna.drop/overall.drop * 100, 2)
 
-# Summarise NPP use (LTW)
+# Summarise NPP use (LTW) [%]
 ltw %>% 
   group_by(period) %>% 
   filter(!is.na(value)) %>%
@@ -298,7 +378,7 @@ ltw %>%
             q.975 = quantile(value, .975, na.rm = T) %>% signif(2),
             n = n())
 
-# Summarise carbon consumption (all)
+# Summarise carbon consumption (all) [MgC / km2 / year]
 consumption.df %>% 
   group_by(period) %>%
   summarise(mean = mean(value, na.rm = T) %>% signif(2),
@@ -307,3 +387,4 @@ consumption.df %>%
             q.025 = quantile(value, .025, na.rm = T) %>% signif(2),
             q.975 = quantile(value, .975, na.rm = T) %>% signif(2),
             n = n())
+

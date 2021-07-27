653904060973ca58b97be7c590ab48749121d8ea
rasmus87 <rasmus.iwfly@gmail.com>
2021-07-27 15:51:06 +0200

    Move total global consumption estimates to the load data script

--- scripts/3.0_load.data.R
+++ scripts/3.0_load.data.R
@@ -388,3 +388,40 @@ consumption.df %>%
             q.975 = quantile(value, .975, na.rm = T) %>% signif(2),
             n = n())
 
+
+
+
+# Calculate total global consumption --------------------------------------
+# Units:  
+# [MgC / (km2 * year) * m2 * km2 / 10^6 m2 * 10^6 g / Mg * Pg / 10^15 g] = [PgC / year]
+tot.current <- sum(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+tot.pres.nat <- sum(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+diff <- tot.pres.nat - tot.current
+
+tot.current.med <- median(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+tot.pres.nat.med <- median(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+tot.current.mean <- mean(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+tot.pres.nat.mean <- mean(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
+
+tot.current.lw <- sum(current.consumption.map.lw[] * prod(res(current.consumption.map.lw))/10^6, na.rm = T) * 10^6 / 10^15
+tot.pres.nat.lw <- sum(present.natural.consumption.map.lw[] * prod(res(current.consumption.map.lw))/10^6, na.rm = T) * 10^6 / 10^15
+diff.lw <- tot.pres.nat.lw - tot.current.lw
+
+tot.current.hi <- sum(current.consumption.map.hi[] * prod(res(current.consumption.map.hi))/10^6, na.rm = T) * 10^6 / 10^15
+tot.pres.nat.hi <- sum(present.natural.consumption.map.hi[] * prod(res(current.consumption.map.hi))/10^6, na.rm = T) * 10^6 / 10^15
+diff.hi <- tot.pres.nat.hi - tot.current.hi
+
+paste0("Current consumption: ", signif(tot.current, 2), " Pg Carbon / year (95%-CI: ", signif(tot.current.lw, 2), "-", signif(tot.current.hi, 2) ,")")
+paste0("Present natural consumption: ", signif(tot.pres.nat, 2), " Pg Carbon / year (95%-CI: ", signif(tot.pres.nat.lw, 2), "-", signif(tot.pres.nat.hi, 2) ,")")
+paste0("Difference: ", signif(diff, 2), " Pg Carbon / year (95%-CI: ", signif(diff.lw, 2), "-", signif(diff.hi, 2) ,")")
+
+
+# As fraction on NPP [%]
+
+# Total NPP [Pg]
+tot.npp <- sum(npp[] * prod(res(npp))/10^6, na.rm = T)*10^6/10^15
+tot.npp
+# Percentage released (Doughty says 2.2-5.3)
+(tot.pres.nat - tot.current)/tot.npp * 100
+(tot.pres.nat.lw - tot.current.lw)/tot.npp * 100
+(tot.pres.nat.hi - tot.current.hi)/tot.npp * 100
--- scripts/3.2_map.consumption.R.R
+++ scripts/3.2_map.consumption.R.R
@@ -1,45 +1,5 @@
 # Run after 3.0_load.data.R
 
-detach(package:cowplot)
-
-### CACULATE TOTALT GLOBAL CONSUMPTION >>>
-# Units:  
-# [MgC / (km2 * year) * m2 * km2 / 10^6 m2 * 10^6 g / Mg * Pg / 10^15 g] = 
-# [PgC / year]
-tot.current <- sum(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-tot.pres.nat <- sum(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-diff <- tot.pres.nat - tot.current
-
-tot.current.med <- median(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-tot.pres.nat.med <- median(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-tot.current.mean <- mean(current.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-tot.pres.nat.mean <- mean(present.natural.consumption.map[] * prod(res(current.consumption.map))/10^6, na.rm = T) * 10^6 / 10^15
-
-tot.current.lw <- sum(current.consumption.map.lw[] * prod(res(current.consumption.map.lw))/10^6, na.rm = T) * 10^6 / 10^15
-tot.pres.nat.lw <- sum(present.natural.consumption.map.lw[] * prod(res(current.consumption.map.lw))/10^6, na.rm = T) * 10^6 / 10^15
-diff.lw <- tot.pres.nat.lw - tot.current.lw
-
-tot.current.hi <- sum(current.consumption.map.hi[] * prod(res(current.consumption.map.hi))/10^6, na.rm = T) * 10^6 / 10^15
-tot.pres.nat.hi <- sum(present.natural.consumption.map.hi[] * prod(res(current.consumption.map.hi))/10^6, na.rm = T) * 10^6 / 10^15
-diff.hi <- tot.pres.nat.hi - tot.current.hi
-
-paste0("Current consumption: ", signif(tot.current, 2), " Pg Carbon / year (95%-CI: ", signif(tot.current.lw, 2), "-", signif(tot.current.hi, 2) ,")")
-paste0("Present natural consumption: ", signif(tot.pres.nat, 2), " Pg Carbon / year (95%-CI: ", signif(tot.pres.nat.lw, 2), "-", signif(tot.pres.nat.hi, 2) ,")")
-paste0("Difference: ", signif(diff, 2), " Pg Carbon / year (95%-CI: ", signif(diff.lw, 2), "-", signif(diff.hi, 2) ,")")
-### CACULATE TOTALT GLOBAL CONSUMPTION |||
-
-#### CONSUMPTION OF NPP (%) >>>
-# Of total (Pg)
-tot.npp <- sum(npp[] * prod(res(npp))/10^6, na.rm = T)*10^6/10^15
-tot.npp
-# Percentage released (Doughty says 2.2-5.3)
-(tot.pres.nat - tot.current)/tot.npp * 100
-(tot.pres.nat.lw - tot.current.lw)/tot.npp * 100
-(tot.pres.nat.hi - tot.current.hi)/tot.npp * 100
-#### CONSUMPTION OF NPP (%) |||
-
-
-
 ### Carbon consumption map [MgC / km2 / year] >>>
 current.consumption.plot <- ggplot(current.consumption.df, aes(x = x, y = y, fill = value)) +
   facet_grid(period ~ .) +

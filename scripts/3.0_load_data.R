a16a5ce01ea7121005399fc8e389ba197d0eb548
rasmus87 <rasmus.iwfly@gmail.com>
2021-11-11 17:38:56 +0100

    Move biome and realm loading to 3.0 load instead of 3.4 violin

--- scripts/3.0_load_data.R
+++ scripts/3.0_load_data.R
@@ -22,7 +22,7 @@ df <- read_csv("builds/data.csv", col_types = cols())
 # Load consumption data
 consumption.samples <- read_csv("builds/sampled.consumption.distribution.kgC.yr.km2.csv", col_types = cols()) # [kgC / (km2 * year)]
 
-# Summarise consumption
+# Summarize consumption
 consumption.summary <- consumption.samples %>%
   transmute(Binomial.1.2,
             mean = rowMeans(.[, -1]),
@@ -39,6 +39,24 @@ consumption <- consumption.summary %>%
          ci.hi.plant = ci.hi * df$Diet.Plant/100)
 
 
+# Load density data
+density.samples <- read_csv("builds/sampled.density.distribution.kgC.yr.km2.csv", col_types = cols()) # [kgC / (km2 * year)]
+# Alignment sanity check 
+stopifnot(all(density.samples$Binomial.1.2 == df$Binomial.1.2))
+
+# Transform to biomass
+mass.samples <- density.samples[, -1] * as.numeric(df$Mass.g)/1e6 # [Mg / km2]
+
+mass.samples <- mass.samples %>% as_tibble() %>%  mutate(Binomial.1.2 = df$Binomial.1.2, .before = "sample.1")
+
+# Summarize consumption
+mass.summary <- mass.samples %>%
+  transmute(Binomial.1.2,
+            median = apply(.[, -1], 1, median),
+            ci.lw = apply(.[, -1], 1, quantile, probs = .025),
+            ci.hi = apply(.[, -1], 1, quantile, probs = .975))
+
+
 
 
 # Load species maps and calculate consumption maps ------------------------
@@ -49,11 +67,15 @@ current.consumption <- current.maps * consumption$Q.plant
 current.consumption.lw <- current.maps * consumption$ci.lw.plant
 current.consumption.hi <- current.maps * consumption$ci.hi.plant
 
+current.mass <- current.maps * mass.summary$median
+
 # Present natural maps
 present.natural.maps <- read_rds("builds/present.natural.maps.filtered.edge.lim.rds")
 present.natural.consumption <- present.natural.maps * consumption$Q.plant
 present.natural.consumption.lw <- present.natural.maps * consumption$ci.lw.plant
 present.natural.consumption.hi <- present.natural.maps * consumption$ci.hi.plant
+
+present.natural.mass <- present.natural.maps * mass.summary$median
 
 # Base map
 base.map <- raster("builds/base_map.tif")
@@ -72,7 +94,8 @@ present.natural.consumption.map.lw <- base.map
 current.consumption.map.hi <- base.map
 present.natural.consumption.map.hi <- base.map
 
-
+current.mass.map <- base.map
+present.natural.mass.map <- base.map
 
 # Calculate total consumption per grid cell -------------------------------
 # Summarize consumption per grid-cell
@@ -83,6 +106,10 @@ current.consumption.map[] <- colSums(current.consumption) / 10^3
 # Remove oceans
 current.consumption.map <- current.consumption.map * land
 # current.consumption.map[current.consumption.map == 0] <- NA
+
+# Current mass
+current.mass.map[] <- colSums(current.mass)
+current.mass.map <- current.mass.map * land
 
 # Current maps CI
 current.consumption.map.lw[] <- colSums(current.consumption.lw) / 10^3
@@ -108,6 +135,10 @@ present.natural.consumption.map[] <- colSums(present.natural.consumption) / 10^3
 # Remove oceans
 present.natural.consumption.map <- present.natural.consumption.map * land
 # present.natural.consumption.map[present.natural.consumption.map == 0] <- NA
+
+# Current mass
+present.natural.mass.map[] <- colSums(present.natural.mass)
+present.natural.mass.map <- present.natural.mass.map * land
 
 # Present natural maps CI
 present.natural.consumption.map.lw[] <- colSums(present.natural.consumption.lw) / 10^3
@@ -201,6 +232,8 @@ present.natural.consumption.map[remove.areas] <- NA
 current.megafauna.consumption.map[remove.areas] <- NA
 present.natural.megafauna.consumption.map[remove.areas] <- NA
 
+current.mass.map[remove.areas] <- NA
+present.natural.mass.map[remove.areas] <- NA
 
 
 # Prepare datasets for plotting ----------------------------------
@@ -266,6 +299,27 @@ change.df <- change %>%
             y, 
             period = "Difference")
 
+
+# Current mass
+current.mass.df <- current.mass.map %>%
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Current")
+# Present natural mass
+present.natural.mass.df <- present.natural.mass.map %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(value = .[[1]],
+            x, 
+            y, 
+            period = "Present natural")
+
+# Combined mass
+mass.df <- bind_rows(current.mass.df, 
+                     present.natural.mass.df)
 
 # Turn NPP use into data.frames for plotting (all)
 # [%]
@@ -359,4 +413,174 @@ colnames(ltw) <- c("value", "x", "y")
 ltw <- ltw[, -1] %>% 
   left_join(npp.use, by = c("x", "y"))
 
+
+
+
+
+# Load WWF realms and biomes --------------------------------------
+
+wwf.realm <- base.map
+wwf.realm[] <- raster("data/wwf_terr_ecos_realm_raster.tif")[]
+wwf.realm.names <- read_csv("data/wwf_realm_names.csv")
+
+wwf.biome <- base.map
+wwf.biome[] <- raster("data/wwf_terr_ecos_biome_raster.tif")[]
+wwf.biome.names <- read_csv("data/wwf_biome_names.csv")
+
+na.cells <- which(wwf.realm[] == 4 | wwf.biome[] %in% c(98, 99, 128) | is.na(wwf.realm[]) | is.na(wwf.biome[]))
+
+# Load WWF realms
+wwf.realm[na.cells] <- NA
+wwf.realm <- wwf.realm %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(wwf_realm = .[[1]],
+            x, 
+            y) %>% 
+  left_join(wwf.realm.names, by = "wwf_realm")
+
+# ggplot(wwf.realm, aes(x = x, y = y, fill = name)) +
+#   geom_tile() +
+#   scale_fill_viridis(name = "WWF Realm", na.value = "pink", discrete = T, option = "D") +
+#   theme_map() +
+#   geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
+#   coord_sf(ylim = range(land.df$y), xlim = range(land.df$x) * 1.02, expand = FALSE)
+
+# Load WWF biomes
+wwf.biome[na.cells] <- NA
+wwf.biome <- wwf.biome %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(wwf_biome = .[[1]],
+            x, 
+            y) %>% 
+  left_join(wwf.biome.names, by = "wwf_biome")
+
+# ggplot(wwf.biome, aes(x = x, y = y, fill = name)) +
+#   geom_tile() +
+#   scale_fill_viridis(name = "WWF biome", na.value = "pink", discrete = T, option = "D") +
+#   theme_map() +
+#   theme(legend.background = element_blank()) +
+#   geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
+#   coord_sf(ylim = range(land.df$y), xlim = range(land.df$x) * 1.02, expand = FALSE)
+
+
+# Remove madagascar and other excluded regions ----------------------------
+
+# Madagascar cells
+madagascar <- c(33345, 33346, 33347, 33348, 33349, 33350, 33351, 33352, 33353, 
+                33705, 33706, 33707, 33708, 33709, 33710, 33711, 33712, 33713, 
+                34065, 34066, 34067, 34068, 34069, 34070, 34071, 34072, 34073, 
+                34425, 34426, 34427, 34428, 34429, 34430, 34431, 34432, 34433,
+                34785, 34786, 34787, 34788, 34789, 34790, 34791, 34792, 34793, 
+                35145, 35146, 35147, 35148, 35149, 35150, 35151, 35152, 35153,
+                35505, 35506, 35507, 35508, 35509, 35510, 35511, 35512, 35513, 
+                35865, 35866, 35867, 35868, 35869, 35870, 35871, 35872, 35873,
+                36225, 36226, 36227, 36228, 36229, 36230, 36231, 36232, 36233,
+                36585, 36586, 36587, 36588, 36589, 36590, 36591, 36592, 36593, 
+                36945, 36946, 36947, 36948, 36949, 36950, 36951, 36952, 36953, 
+                37305, 37306, 37307, 37308, 37309, 37310, 37311, 37312, 37313, 
+                37665, 37666, 37667, 37668, 37669, 37670, 37671, 37672, 37673, 
+                38025, 38026, 38027, 38028, 38029, 38030, 38031, 38032, 38033, 
+                38385, 38386, 38387, 38388, 38389, 38390, 38391, 38392, 38393, 
+                38745, 38746, 38747, 38748, 38749, 38750, 38751, 38752, 38753,
+                39105, 39106, 39107, 39108, 39109, 39110, 39111, 39112, 39113, 
+                39465, 39466, 39467, 39468, 39469, 39470, 39471, 39472, 39473, 
+                39825, 39826, 39827, 39828, 39829, 39830, 39831, 39832, 39833)
+
+na.cells.expanded <- which(wwf.realm[] %in% c(4, 7) | wwf.biome[] %in% c(98, 99, 128) | is.na(wwf.realm[]) | is.na(wwf.biome[]))
+madagascar <- c(33345, 33346, 33347, 33348, 33349, 33350, 33351, 33352, 33353, 33705, 33706, 33707, 33708, 33709, 33710, 33711, 33712, 33713, 34065, 34066, 34067, 34068, 34069, 34070, 34071, 34072, 34073, 34425, 34426, 34427, 34428, 34429, 34430, 34431, 34432, 34433, 34785, 34786, 34787, 34788, 34789, 34790, 34791, 34792, 34793, 35145, 35146, 35147, 35148, 35149, 35150, 35151, 35152, 35153, 35505, 35506, 35507, 35508, 35509, 35510, 35511, 35512, 35513, 35865, 35866, 35867, 35868, 35869, 35870, 35871, 35872, 35873, 36225, 36226, 36227, 36228, 36229, 36230, 36231, 36232, 36233, 36585, 36586, 36587, 36588, 36589, 36590, 36591, 36592, 36593, 36945, 36946, 36947, 36948, 36949, 36950, 36951, 36952, 36953, 37305, 37306, 37307, 37308, 37309, 37310, 37311, 37312, 37313, 37665, 37666, 37667, 37668, 37669, 37670, 37671, 37672, 37673, 38025, 38026, 38027, 38028, 38029, 38030, 38031, 38032, 38033, 38385, 38386, 38387, 38388, 38389, 38390, 38391, 38392, 38393, 38745, 38746, 38747, 38748, 38749, 38750, 38751, 38752, 38753, 39105, 39106, 39107, 39108, 39109, 39110, 39111, 39112, 39113, 39465, 39466, 39467, 39468, 39469, 39470, 39471, 39472, 39473, 39825, 39826, 39827, 39828, 39829, 39830, 39831, 39832, 39833)
+na.cells.expanded <- c(na.cells.expanded, madagascar)
+na.cells.expanded <- c(na.cells.expanded, remove.areas)
+na.cells.expanded <- unique(na.cells.expanded)
+
+wwf.realm <- base.map
+wwf.realm[] <- raster("data/wwf_terr_ecos_realm_raster.tif")[]
+wwf.realm.names <- read_csv("data/wwf_realm_names.csv")
+
+wwf.biome <- base.map
+wwf.biome[] <- raster("data/wwf_terr_ecos_biome_raster.tif")[]
+wwf.biome.names <- read_csv("data/wwf_biome_names.csv")
+
+wwf.realm[na.cells.expanded] <- NA
+wwf.realm <- wwf.realm %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(wwf_realm = .[[1]],
+            x, 
+            y) %>% 
+  left_join(wwf.realm.names, by = "wwf_realm") %>% 
+  transmute(x, y, realm = name)
+
+wwf.biome[na.cells.expanded] <- NA
+wwf.biome <- wwf.biome %>% 
+  as("SpatialPixelsDataFrame") %>% 
+  as_tibble() %>% 
+  transmute(wwf_biome = .[[1]],
+            x, 
+            y) %>% 
+  left_join(wwf.biome.names, by = "wwf_biome") %>% 
+  transmute(x, y, biome = name)
+
+eco.units <- left_join(wwf.realm, wwf.biome, by = c("x", "y"))
+
+
+# Select the system we keep and look at -----------------------------------
+
+# Remove ecosystems we are not interested in
+leave.out <- c("Tropical and Subtropical Coniferous Forests", "Flooded Grasslands and Savannas",
+               "Montane Grasslands and Shrublands", "Tundra", "Deserts and Xeric Shrublands", "Mangroves")
+eco.units <- eco.units %>%
+  filter(!biome %in% leave.out)
+
+# Combine connected realms and biomes
+eco.units <- eco.units %>% 
+  mutate(realm = ifelse(realm %in% c("Nearctic", "Neotropic"), "Americas", realm)) %>% 
+  mutate(biome = ifelse(biome %in% c("Temperate Broadleaf and Mixed Forests", "Temperate Coniferous Forests"), "Temperate Forests", biome)) %>% 
+  mutate(biome = ifelse(biome %in% c("Tropical and Subtropical Dry Broadleaf Forests", "Tropical and subtropical grasslands, savannas, and shrublands"), "Tropical and Subtropical Grasslands to Forests", biome)) %>% 
+  mutate(eco.unit = paste(realm, biome, sep = " - "))
+
+# See which regions are left
+eco.units %>% count(eco.unit) %>% print(n = 100)
+
+# Filter to an intersting subset with most cells
+keep.units <- c(
+  "Afrotropic - Tropical and Subtropical Grasslands to Forests",
+  "Afrotropic - Tropical and Subtropical Moist Broadleaf Forests",
+  "Australasia - Mediterranean Forests, Woodlands, and Scrub",
+  "Australasia - Temperate Forests",
+  "Australasia - Temperate Grasslands, Savannas, and Shrublands",
+  "Australasia - Tropical and Subtropical Grasslands to Forests",
+  "Australasia - Tropical and Subtropical Moist Broadleaf Forests",
+  "Indomalaya - Tropical and Subtropical Grasslands to Forests",
+  "Indomalaya - Tropical and Subtropical Moist Broadleaf Forests",
+  "Americas - Boreal Forests/Taiga",
+  "Americas - Temperate Forests",
+  "Americas - Temperate Grasslands, Savannas, and Shrublands",
+  "Americas - Tropical and Subtropical Grasslands to Forests",
+  "Americas - Tropical and Subtropical Moist Broadleaf Forests",
+  "Palearctic - Boreal Forests/Taiga",
+  "Palearctic - Mediterranean Forests, Woodlands, and Scrub",
+  "Palearctic - Temperate Forests",
+  "Palearctic - Temperate Grasslands, Savannas, and Shrublands")
+
+eco.units <- eco.units %>% filter(eco.unit %in% keep.units)
+eco.units %>% count(eco.unit)
+
+# Shorten biome names
+eco.units$biome <- str_replace(eco.units$biome, "Temperate Grasslands, Savannas, and Shrublands", "Temperate Grasslands to Shrublands")
+eco.units$biome <- str_replace(eco.units$biome, "Mediterranean Forests, Woodlands, and Scrub", "Mediterranean Forests to Scrub")
+
+# Plot Supplementary figure of ecoregions
+cols <- colorspace::qualitative_hcl(palette = "Dark 3", n = 5)
+breaks = c("Americas", "Afrotropic", "Palearctic", "Indomalaya", "Australasia")
+eco.units.plot <- ggplot(eco.units, aes(x = x, y = y, fill = realm)) +
+  facet_wrap(vars(biome), nrow = 3) +
+  geom_tile() +
+  scale_fill_manual(name = "Realm", na.value = "pink", values = cols, breaks = breaks) +
+  theme_map() +
+  theme(legend.position = "bottom", legend.justification = NULL)  +
+  geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
+  coord_sf(ylim = range(land.df$y) * 1.02, xlim = range(land.df$x) * 1.02, expand = FALSE)
+ggsave("./output/fig_ecoregions.png", eco.units.plot, width = 183, height = 137, units = "mm", dpi = 600)
 
--- scripts/3.4_violin_plots.R
+++ scripts/3.4_violin_plots.R
@@ -2,175 +2,6 @@
 # 28/07-2021 Rasmus Ø Pedersen
 
 
-# Test look at WWF realms and biomes --------------------------------------
-
-wwf.realm <- base.map
-wwf.realm[] <- raster("data/wwf_terr_ecos_realm_raster.tif")[]
-wwf.realm.names <- read_csv("data/wwf_realm_names.csv")
-
-wwf.biome <- base.map
-wwf.biome[] <- raster("data/wwf_terr_ecos_biome_raster.tif")[]
-wwf.biome.names <- read_csv("data/wwf_biome_names.csv")
-
-na.cells <- which(wwf.realm[] == 4 | wwf.biome[] %in% c(98, 99, 128) | is.na(wwf.realm[]) | is.na(wwf.biome[]))
-
-# Load WWF realms
-wwf.realm[na.cells] <- NA
-wwf.realm <- wwf.realm %>% 
-  as("SpatialPixelsDataFrame") %>% 
-  as_tibble() %>% 
-  transmute(wwf_realm = .[[1]],
-            x, 
-            y) %>% 
-  left_join(wwf.realm.names, by = "wwf_realm")
-
-# ggplot(wwf.realm, aes(x = x, y = y, fill = name)) +
-#   geom_tile() +
-#   scale_fill_viridis(name = "WWF Realm", na.value = "pink", discrete = T, option = "D") +
-#   theme_map() +
-#   geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
-#   coord_sf(ylim = range(land.df$y), xlim = range(land.df$x) * 1.02, expand = FALSE)
-
-# Load WWF biomes
-wwf.biome[na.cells] <- NA
-wwf.biome <- wwf.biome %>% 
-  as("SpatialPixelsDataFrame") %>% 
-  as_tibble() %>% 
-  transmute(wwf_biome = .[[1]],
-            x, 
-            y) %>% 
-  left_join(wwf.biome.names, by = "wwf_biome")
-
-# ggplot(wwf.biome, aes(x = x, y = y, fill = name)) +
-#   geom_tile() +
-#   scale_fill_viridis(name = "WWF biome", na.value = "pink", discrete = T, option = "D") +
-#   theme_map() +
-#   theme(legend.background = element_blank()) +
-#   geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
-#   coord_sf(ylim = range(land.df$y), xlim = range(land.df$x) * 1.02, expand = FALSE)
-
-
-# Remove madagascar and other excluded regions ----------------------------
-
-# Madagascar cells
-madagascar <- c(33345, 33346, 33347, 33348, 33349, 33350, 33351, 33352, 33353, 
-                33705, 33706, 33707, 33708, 33709, 33710, 33711, 33712, 33713, 
-                34065, 34066, 34067, 34068, 34069, 34070, 34071, 34072, 34073, 
-                34425, 34426, 34427, 34428, 34429, 34430, 34431, 34432, 34433,
-                34785, 34786, 34787, 34788, 34789, 34790, 34791, 34792, 34793, 
-                35145, 35146, 35147, 35148, 35149, 35150, 35151, 35152, 35153,
-                35505, 35506, 35507, 35508, 35509, 35510, 35511, 35512, 35513, 
-                35865, 35866, 35867, 35868, 35869, 35870, 35871, 35872, 35873,
-                36225, 36226, 36227, 36228, 36229, 36230, 36231, 36232, 36233,
-                36585, 36586, 36587, 36588, 36589, 36590, 36591, 36592, 36593, 
-                36945, 36946, 36947, 36948, 36949, 36950, 36951, 36952, 36953, 
-                37305, 37306, 37307, 37308, 37309, 37310, 37311, 37312, 37313, 
-                37665, 37666, 37667, 37668, 37669, 37670, 37671, 37672, 37673, 
-                38025, 38026, 38027, 38028, 38029, 38030, 38031, 38032, 38033, 
-                38385, 38386, 38387, 38388, 38389, 38390, 38391, 38392, 38393, 
-                38745, 38746, 38747, 38748, 38749, 38750, 38751, 38752, 38753,
-                39105, 39106, 39107, 39108, 39109, 39110, 39111, 39112, 39113, 
-                39465, 39466, 39467, 39468, 39469, 39470, 39471, 39472, 39473, 
-                39825, 39826, 39827, 39828, 39829, 39830, 39831, 39832, 39833)
-
-na.cells.expanded <- which(wwf.realm[] %in% c(4, 7) | wwf.biome[] %in% c(98, 99, 128) | is.na(wwf.realm[]) | is.na(wwf.biome[]))
-madagascar <- c(33345, 33346, 33347, 33348, 33349, 33350, 33351, 33352, 33353, 33705, 33706, 33707, 33708, 33709, 33710, 33711, 33712, 33713, 34065, 34066, 34067, 34068, 34069, 34070, 34071, 34072, 34073, 34425, 34426, 34427, 34428, 34429, 34430, 34431, 34432, 34433, 34785, 34786, 34787, 34788, 34789, 34790, 34791, 34792, 34793, 35145, 35146, 35147, 35148, 35149, 35150, 35151, 35152, 35153, 35505, 35506, 35507, 35508, 35509, 35510, 35511, 35512, 35513, 35865, 35866, 35867, 35868, 35869, 35870, 35871, 35872, 35873, 36225, 36226, 36227, 36228, 36229, 36230, 36231, 36232, 36233, 36585, 36586, 36587, 36588, 36589, 36590, 36591, 36592, 36593, 36945, 36946, 36947, 36948, 36949, 36950, 36951, 36952, 36953, 37305, 37306, 37307, 37308, 37309, 37310, 37311, 37312, 37313, 37665, 37666, 37667, 37668, 37669, 37670, 37671, 37672, 37673, 38025, 38026, 38027, 38028, 38029, 38030, 38031, 38032, 38033, 38385, 38386, 38387, 38388, 38389, 38390, 38391, 38392, 38393, 38745, 38746, 38747, 38748, 38749, 38750, 38751, 38752, 38753, 39105, 39106, 39107, 39108, 39109, 39110, 39111, 39112, 39113, 39465, 39466, 39467, 39468, 39469, 39470, 39471, 39472, 39473, 39825, 39826, 39827, 39828, 39829, 39830, 39831, 39832, 39833)
-na.cells.expanded <- c(na.cells.expanded, madagascar)
-na.cells.expanded <- c(na.cells.expanded, remove.areas)
-na.cells.expanded <- unique(na.cells.expanded)
-
-wwf.realm <- base.map
-wwf.realm[] <- raster("data/wwf_terr_ecos_realm_raster.tif")[]
-wwf.realm.names <- read_csv("data/wwf_realm_names.csv")
-
-wwf.biome <- base.map
-wwf.biome[] <- raster("data/wwf_terr_ecos_biome_raster.tif")[]
-wwf.biome.names <- read_csv("data/wwf_biome_names.csv")
-
-wwf.realm[na.cells.expanded] <- NA
-wwf.realm <- wwf.realm %>% 
-  as("SpatialPixelsDataFrame") %>% 
-  as_tibble() %>% 
-  transmute(wwf_realm = .[[1]],
-            x, 
-            y) %>% 
-  left_join(wwf.realm.names, by = "wwf_realm") %>% 
-  transmute(x, y, realm = name)
-
-wwf.biome[na.cells.expanded] <- NA
-wwf.biome <- wwf.biome %>% 
-  as("SpatialPixelsDataFrame") %>% 
-  as_tibble() %>% 
-  transmute(wwf_biome = .[[1]],
-            x, 
-            y) %>% 
-  left_join(wwf.biome.names, by = "wwf_biome") %>% 
-  transmute(x, y, biome = name)
-
-eco.units <- left_join(wwf.realm, wwf.biome, by = c("x", "y"))
-
-
-# Select the system we keep and look at -----------------------------------
-
-# Remove ecosystems we are not interested in
-leave.out <- c("Tropical and Subtropical Coniferous Forests", "Flooded Grasslands and Savannas",
-               "Montane Grasslands and Shrublands", "Tundra", "Deserts and Xeric Shrublands", "Mangroves")
-eco.units <- eco.units %>%
-  filter(!biome %in% leave.out)
-
-# Combine connected realms and biomes
-eco.units <- eco.units %>% 
-  mutate(realm = ifelse(realm %in% c("Nearctic", "Neotropic"), "Americas", realm)) %>% 
-  mutate(biome = ifelse(biome %in% c("Temperate Broadleaf and Mixed Forests", "Temperate Coniferous Forests"), "Temperate Forests", biome)) %>% 
-  mutate(biome = ifelse(biome %in% c("Tropical and Subtropical Dry Broadleaf Forests", "Tropical and subtropical grasslands, savannas, and shrublands"), "Tropical and Subtropical Grasslands to Forests", biome)) %>% 
-  mutate(eco.unit = paste(realm, biome, sep = " - "))
-
-# See which regions are left
-eco.units %>% count(eco.unit) %>% print(n = 100)
-
-# Filter to an intersting subset with most cells
-keep.units <- c(
-"Afrotropic - Tropical and Subtropical Grasslands to Forests",
-"Afrotropic - Tropical and Subtropical Moist Broadleaf Forests",
-"Australasia - Mediterranean Forests, Woodlands, and Scrub",
-"Australasia - Temperate Forests",
-"Australasia - Temperate Grasslands, Savannas, and Shrublands",
-"Australasia - Tropical and Subtropical Grasslands to Forests",
-"Australasia - Tropical and Subtropical Moist Broadleaf Forests",
-"Indomalaya - Tropical and Subtropical Grasslands to Forests",
-"Indomalaya - Tropical and Subtropical Moist Broadleaf Forests",
-"Americas - Boreal Forests/Taiga",
-"Americas - Temperate Forests",
-"Americas - Temperate Grasslands, Savannas, and Shrublands",
-"Americas - Tropical and Subtropical Grasslands to Forests",
-"Americas - Tropical and Subtropical Moist Broadleaf Forests",
-"Palearctic - Boreal Forests/Taiga",
-"Palearctic - Mediterranean Forests, Woodlands, and Scrub",
-"Palearctic - Temperate Forests",
-"Palearctic - Temperate Grasslands, Savannas, and Shrublands")
-
-eco.units <- eco.units %>% filter(eco.unit %in% keep.units)
-eco.units %>% count(eco.unit)
-
-# Shorten biome names
-eco.units$biome <- str_replace(eco.units$biome, "Temperate Grasslands, Savannas, and Shrublands", "Temperate Grasslands to Shrublands")
-eco.units$biome <- str_replace(eco.units$biome, "Mediterranean Forests, Woodlands, and Scrub", "Mediterranean Forests to Scrub")
-
-# Plot Supplementary figure of ecoregions
-cols <- colorspace::qualitative_hcl(palette = "Dark 3", n = 5)
-breaks = c("Americas", "Afrotropic", "Palearctic", "Indomalaya", "Australasia")
-eco.units.plot <- ggplot(eco.units, aes(x = x, y = y, fill = realm)) +
-  facet_wrap(vars(biome), nrow = 3) +
-  geom_tile() +
-  scale_fill_manual(name = "Realm", na.value = "pink", values = cols, breaks = breaks) +
-  theme_map() +
-  theme(legend.position = "bottom", legend.justification = NULL)  +
-  geom_sf(data = world.map, inherit.aes = F, col = "black", fill = NA, lwd = .25) +
-  coord_sf(ylim = range(land.df$y) * 1.02, xlim = range(land.df$x) * 1.02, expand = FALSE)
-ggsave("./output/fig_ecoregions.png", eco.units.plot, width = 183, height = 137, units = "mm", dpi = 600)
-
-
-
 # Setup data for plots ----------------------------------------------------
 
 # a: Eco-unit consumption [MgC / yr /km2]
@@ -193,6 +24,12 @@ ltw.eco.unit.npp.consumption <- eco.units %>%
   bind_rows(eco.unit.npp.consumption %>%
               count(realm, biome, period) %>%
               dplyr::select(1:3))
+
+# d: Eco-unit mass [Mg / km2]
+eco.unit.mass <- mass.df %>%
+  left_join(eco.units, by = c("x", "y")) %>%
+  mutate(period = fct_relevel(period, "Present natural")) %>%
+  filter(!is.na(period))
   
 # Setup period colors
 period.colors <- c("Present natural" = "#b2df8a", "Current" = "#a6cee3")
@@ -293,7 +130,40 @@ p3c <- ggplot(ltw.eco.unit.npp.consumption, aes(period, value, fill = period)) +
     panel.grid.major = element_blank()
   )
 
+# c: Total mammal mass [Mg / km2]
+p3d <- ggplot(eco.unit.mass, aes(period, value, fill = period)) +
+  geom_violin(width = 0.7, scale = "width", linetype = "blank") +
+  geom_boxplot(
+    width = 0.10,
+    col = "black",
+    position = position_dodge(width = 0.7),
+    outlier.shape = 20, 
+    outlier.size = 0.5,
+    show.legend = FALSE, 
+    fill = "black",
+  ) +
+  stat_summary(
+    fun = median,
+    geom = "point",
+    col = "white",
+    size = 1,
+    shape = 20,
+    position = position_dodge(width = 0.7)) +
+  theme_R() +
+  ylab(expression((a)~Mammal~mass~(Mg/km^2))) +
+  xlab(NULL) +
+  scale_fill_manual(values = period.colors, name = "Period") +
+  theme(
+    strip.text.x = element_text(size = 5.4),
+    axis.text.x = element_text(angle = 30, vjust = .8, hjust = .8),
+    legend.position = "none",
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank()
+  )
+
 p3 <- cowplot::plot_grid(p3a, p3b, p3c, nrow = 1)
+p3.alt <- cowplot::plot_grid(p3a, p3b, p3c, p3d, nrow = 1)
+p3 <- p3.alt
 
 if(full) {
   ggsave("./output/fig3_global_violins_full.png", p3, width = 89, height = 89, units = "mm", dpi = 600, scale = 1.5)
